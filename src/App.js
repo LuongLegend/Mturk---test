86f3b66cd6b44b3e805baa2950248247f5b11980
luongnv <luongnv@icts.vn>
2021-10-26 11:20:56 +0700

    mturk submission

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createRef } from "react";
 
 function App() {
+  const formMturk = createRef();
+  const inputAssignmentID = createRef();
+  const search = window.location.search;
+  const params = new URLSearchParams(search);
+  const assignmentId = params.get("assignmentId");
+  const handleSubmit = () => {
+    if(!assignmentId || assignmentId === 'ASSIGNMENT_ID_NOT_AVAILABLE') {
+      alert('You need accept to do this task');
+    }
+    else {
+      //call api from assocition or do anything.
+      inputAssignmentID.current.value = assignmentId;
+      formMturk.current.submit();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ marginTop: "150px" }}>
+      Your worker Id:
+      <form
+        method="post"
+        id="mturk_form"
+        action="https://www.mturk.com/mturk/externalSubmit"
+        ref={formMturk}
+      >
+        <label for="color">Color: </label>
+        <input type="text" id="color" name="color" />
+        <input type="hidden" id="inputAssignmentId" name="assignmentId" ref={inputAssignmentID}/>
+      </form>
+      <input value="submit" type="button" onClick={handleSubmit} />
     </div>
   );
 }
